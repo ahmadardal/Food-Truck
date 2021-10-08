ca110869262faa3108eedd0c0e6452e1ccd62fe1
Rai <raibr05@hotmail.com>
2021-10-08 14:19:53 +0200

    Logga In Act Previw!

---
+++ app/src/main/java/com/example/food_trock/inLoggningAct.kt
@@ -0,0 +1,11 @@
+package com.example.food_trock
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class inLoggningAct : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_in_loggning)
+    }
+}
