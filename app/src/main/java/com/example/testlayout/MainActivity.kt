013b4d21ecc7a4491cce37cceb16f637e2b2ddb0
Rai <raibr05@hotmail.com>
2021-10-10 15:05:52 +0200

    Design Act

---
+++ app/src/main/java/com/example/testlayout/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.testlayout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
