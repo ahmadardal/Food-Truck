2b12c2a5c57e5c735e86bfa9ada080729e1313ad
crystalii <77482900+crystalii@users.noreply.github.com>
2021-10-10 22:08:02 +0200

    Updates

---
+++ app/src/main/java/com/example/food_trock/StoreFragment.kt
@@ -0,0 +1,20 @@
+package com.example.food_trock
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class StoreFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+        val view = inflater.inflate()
+    }
+
+}
