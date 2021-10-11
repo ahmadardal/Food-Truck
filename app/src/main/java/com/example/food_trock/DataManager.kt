2123a4218dec42bb9d745c3609c4e9622785a9e5
crystalii <77482900+crystalii@users.noreply.github.com>
2021-10-12 00:04:21 +0200

    Update data

--- app/src/main/java/com/example/food_trock/DataManager.kt
+++ app/src/main/java/com/example/food_trock/DataManager.kt
@@ -9,9 +9,9 @@ object DataManager {
     }
 
     private fun setData() {
-        stores.add(Store(R.drawable.hamburger, "Lillans Truck", "En foodtruck med olika slags rätter.", 80, "2.7km", 4))
-        stores.add(Store(R.drawable.hamburger, "Lillans Truck", "En foodtruck med olika slags rätter.", 70, "2.7km", 4))
-        stores.add(Store(R.drawable.hamburger, "Lillans Truck", "En foodtruck med olika slags rätter.", 79, "2.7km", 4))
+        stores.add(Store(R.drawable.hamburger, "Dyra trucken", "En foodtruck med olika slags rätter.", 120, "2.7km", 4))
+        stores.add(Store(R.drawable.hamburger, "Mellan truck", "En foodtruck med olika slags rätter.", 80, "2.7km", 4))
+        stores.add(Store(R.drawable.hamburger, "Billiga Truck", "En foodtruck med olika slags rätter.", 65, "2.7km", 4))
     }
 
 /*    private fun setData(storeImage: Int, storeName: String, storeDescription: String, storePriceClass: String, storeDistance: String, storeRating: Int) {
