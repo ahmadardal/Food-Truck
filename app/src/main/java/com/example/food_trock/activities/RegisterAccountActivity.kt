5e7cd11f4a9bd7dda937fe4aa9129720d5748dcb
RobinXia99 <90704273+RobinXia99@users.noreply.github.com>
2021-10-14 10:00:30 +0200

    layout+file names

--- app/src/main/java/com/example/food_trock/activities/RegisterAccountActivity.kt
+++ app/src/main/java/com/example/food_trock/activities/RegisterAccountActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.food_trock.R
 
-class RegisterAccount : AppCompatActivity() {
+class RegisterAccountActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_account)
