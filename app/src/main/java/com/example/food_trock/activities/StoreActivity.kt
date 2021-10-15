5932ccb26d7a2c0bef2caad7bab49eb7021666e6
RobinXia99 <90704273+RobinXia99@users.noreply.github.com>
2021-10-15 15:09:32 +0200

    loginTest + add store to DB

--- app/src/main/java/com/example/food_trock/activities/StoreActivity.kt
+++ app/src/main/java/com/example/food_trock/activities/StoreActivity.kt
@@ -10,10 +10,17 @@ import com.example.food_trock.R
 import com.example.food_trock.models.Store
 import com.example.food_trock.fragments.StoreFragment
 import com.example.food_trock.adapters.storeAdapter
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class StoreActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
+    lateinit var db: FirebaseFirestore
+    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +30,9 @@ class StoreActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         var storeAdapter = storeAdapter(this, DataManager.stores)
         recyclerView.adapter = storeAdapter
+
+        db = Firebase.firestore
+        auth = Firebase.auth
 
         storeAdapter.setOnItemClickListener(object : storeAdapter.onItemClickListener {
             override fun onItemClick(position: Int) {
@@ -43,15 +53,43 @@ class StoreActivity : AppCompatActivity() {
                 transaction.commit()
 
 
-                Log.e("TEST", "TEST")
-
-
-                var storeName: String = selectedStore.storeName
-                var storePriceClass: Int = selectedStore.storePriceClass
-                var storeDistance: String = selectedStore.storeDistance
 
 
             }
         })
+
+        val raisStore = Store(R.drawable.hamburger,"Rai Pizzeria", 100, "10km",)
+        val email = "rai@test.se"
+        val password = "123456"
+        /*
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { result ->
+            if(result.isSuccessful) {
+                Log.e("TEST","onCreateUser: successful")
+            } else {
+                Log.e("TEST", "onCreateUser: unsuccessful")
+            }
+        }
+
+ */
+        /*
+        val user = auth.currentUser
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { result ->
+            if(result.isSuccessful) {
+                Log.e("TEST","onLogin: successful user: ${user!!.email}")
+
+            }
+        }
+
+         */
+
+        /*
+        if (user != null) {
+            db.collection("users").document(user.uid).collection("truckInfo").add(raisStore)
+        }
+         */
+
+
+
+
     }
 }
