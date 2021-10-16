31c61882ae0f0c966f6ec35d40c80708d7975f26
RobinXia99 <90704273+RobinXia99@users.noreply.github.com>
2021-10-16 22:50:13 +0200

    Revert "Merge pull request #4 from crystalii/CardViewFirestore2"
    This reverts commit 9cb279bb2874aa91745074b8411b68a8bc0206b0, reversing
    changes made to 4cb6b90efd50907ad712b244a0bf75dce7e84282.

--- app/src/main/java/com/example/food_trock/DataManager.kt
+++ app/src/main/java/com/example/food_trock/DataManager.kt
@@ -1,20 +1,13 @@
 package com.example.food_trock
 
 import com.example.food_trock.models.Store
+import com.example.food_trock.models.StoreStatus
 
 object DataManager {
 
     val stores = mutableListOf<Store>()
 
-    init {
-        setData()
-    }
-
-    private fun setData() {
-        stores.add(Store(R.drawable.hamburger, "Dyra trucken", 120, "2.7km", 4))
-        stores.add(Store(R.drawable.hamburger, "Mellan truck",  80, "2.7km", 4))
-        stores.add(Store(R.drawable.hamburger, "Billiga Truck",  65, "2.7km", 4))
-    }
+    val listOfUID = mutableListOf<StoreStatus>()
 
 /*    private fun setData(storeImage: Int, storeName: String, storeDescription: String, storePriceClass: String, storeDistance: String, storeRating: Int) {
         stores.add(Store(storeImage, storeName, storeDescription, storePriceClass, storeDistance, storeRating))
