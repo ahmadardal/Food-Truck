6b87fc9d0d9831ef4ea234e6def33de2e176215f
Housame <housame.oueslati@hotmail.com>
2021-11-07 18:55:36 +0100

    Calculating distance between foodtruck & currentlocation

--- app/src/main/java/com/example/food_trock/adapters/StoreAdapter.kt
+++ app/src/main/java/com/example/food_trock/adapters/StoreAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.food_trock.adapters
 
 import android.content.Context
+import android.location.Location.distanceBetween
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,7 +11,10 @@ import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.food_trock.DataManager.currentLat
+import com.example.food_trock.DataManager.currentLng
 import com.example.food_trock.R
+import com.example.food_trock.firebase.FireStoreClass
 import com.example.food_trock.models.Store
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
@@ -50,7 +54,8 @@ class storeAdapter(val context: Context, val storeList: List<Store> ) :
 
 
 
-       // holder.txtDistance.text = currentItem.storeDistance
+        // holder.txtDistance.text = currentItem.storeDistance
+        holder.txtDistance.text = distanceBetweenInKm(currentLat, currentLng, currentItem.storeLatitude, currentItem.storeLongitude)
        // holder.ratingBar.rating = currentItem.storeRating.toFloat()
 
         if (currentItem.storePriceClass <= 70) {
@@ -61,6 +66,44 @@ class storeAdapter(val context: Context, val storeList: List<Store> ) :
             holder.txtPriceClass.text = "$$$"
 
 
+    }
+
+    /**
+     * A function to calculte distance between current location and Foodtrucks location.
+     */
+    fun distanceBetweenInKm (startLatitude: String,
+                                    startLongitude: String,
+                                    endLatitude: Double,
+                                    endLongitude: Double,
+    ):String {
+        try {
+            val startLat = startLatitude.toDouble()
+            val startLong = startLongitude.toDouble()
+            val endLat = endLatitude
+            val endLong = endLongitude
+
+            val theta = startLong - endLong
+            var dist = Math.sin(deg2rad(startLat)) * Math.sin(deg2rad(endLat)) + Math.cos(deg2rad(startLat)) * Math.cos(deg2rad(endLat)) * Math.cos(deg2rad(theta))
+            dist = Math.acos(dist)
+            dist = rad2deg(dist)
+            dist = dist * 60 * 1.1515
+            dist = dist * 1.609344
+            return dist.toString() + " km"
+        }
+        catch (e: Exception) {
+            return "n/a";
+        }
+
+
+
+
+    }
+    private fun deg2rad(deg: Double): Double {
+        return deg * Math.PI / 180.0
+    }
+
+    private fun rad2deg(rad: Double): Double {
+        return rad * 180.0 / Math.PI
     }
 
     override fun getItemCount(): Int {
